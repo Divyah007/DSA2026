e01abfd6919b2f914fd010c3719a3adb986971ed
Divya Lakshmi V <46861059+Divyah007@users.noreply.github.com>
2026-04-21 01:22:59 +0530

    commited patterna

---
+++ 2.pattern/src/Six.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Six {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter the value for n: ");
+        int n= s.nextInt();
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print((j+1) +" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ * TC: O(N^2)
+ * SC: O(1)
+ * */
